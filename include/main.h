c4576bfd62bc8a7eb76823de27d7768b93d54f10
veredz <verose@live.com>
2018-12-15 01:29:35 +0200

    First commit

---
+++ include/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Verose on 12/14/2018.
+//
+
+#ifndef FREEFROMHOLDERS_BLA_H
+#define FREEFROMHOLDERS_BLA_H
+
+#include <iostream>
+
+
+#endif //FREEFROMHOLDERS_BLA_H
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Verose on 12/14/2018.
+//
+
+#include "../include/main.h"
+
+using namespace std;
+
+
+int main (int argc, char *argv[]) {
+    cout << "Hello World";
+}
